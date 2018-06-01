0e045b26c57e23e7805e20a756a0f4d7c95835b8
Gogs <gogs@fake.local>
2018-06-01 17:56:59 +0800

    测试

--- app/src/main/java/com/cp/wode/qianbao/ZhangBianJiLuFragment.java
+++ app/src/main/java/com/cp/wode/qianbao/ZhangBianJiLuFragment.java
@@ -13,6 +13,7 @@ import utils.tjyutils.common.Common;
 import utils.tjyutils.common.TimeTypeChoose;
 import utils.tjyutils.parent.ParentFragment;
 import utils.wzutils.common.LogTool;
+import utils.wzutils.common.StringTool;
 import utils.wzutils.common.TestData;
 import utils.wzutils.common.UiTool;
 import utils.wzutils.http.HttpUiCallBack;
@@ -109,27 +110,45 @@ public class ZhangBianJiLuFragment extends ParentFragment implements WzRefreshLa
                 setTextView(itemView,R.id.tv_jilu_time,""+qianBaoListBean.cwallet_time);
                 setTextView(itemView,R.id.tv_jilu_yue,"余额:"+qianBaoListBean.cwallet_remain);
 
-
-                String orderNum="";
-                /**
-                 * RECHARGE("充值", 0), EXTRACT("提现", 1), BETTING("投注", 2), BONUS("奖金", 3), REFUND("退款", 4), SHAREBONUS("分享收益", 5), LUCK("抽奖", 6), TEAMBONUS("分红", 7),REPEALOEDER("撤销",8)
-                 只有充值 提现 投注、奖金、、、退款、撤销
-                 */
-                if(qianBaoListBean.cwallet_type==0||qianBaoListBean.cwallet_type==1||qianBaoListBean.cwallet_type==2||qianBaoListBean.cwallet_type==3||qianBaoListBean.cwallet_type==4||qianBaoListBean.cwallet_type==8){
-                    orderNum=" - 订单号"+qianBaoListBean.remark;
+                if(StringTool.notEmpty(qianBaoListBean.spare)){
+                    setTextView(itemView,R.id.tv_jilu_game_name," - "+qianBaoListBean.spare);
+                }else {
+                    setTextView(itemView,R.id.tv_jilu_game_name,"");
                 }
 
 
-                setTextView(itemView,R.id.tv_jilu_order_num,orderNum);
+
+                {//订单号
+                    String orderNum="";
+                    /**
+                     * RECHARGE("充值", 0), EXTRACT("提现", 1), BETTING("投注", 2), BONUS("奖金", 3), REFUND("退款", 4), SHAREBONUS("分享收益", 5), LUCK("抽奖", 6), TEAMBONUS("分红", 7),REPEALOEDER("撤销",8)
+                     只有充值 提现 投注、奖金、、、退款、撤销
+                     */
+                    if(qianBaoListBean.cwallet_type==0||qianBaoListBean.cwallet_type==1||qianBaoListBean.cwallet_type==2||qianBaoListBean.cwallet_type==3||qianBaoListBean.cwallet_type==4||qianBaoListBean.cwallet_type==8){
+                        orderNum="订单号:"+qianBaoListBean.remark;
+                    }
+
+                    TextView tv_jilu_order_num=itemView.findViewById(R.id.tv_jilu_order_num);
+
+                    if(StringTool.isEmpty(orderNum)){
+                        tv_jilu_order_num.setVisibility(View.GONE);
+                    }else {
+                        tv_jilu_order_num.setVisibility(View.VISIBLE);
+                        setTextView(itemView,R.id.tv_jilu_order_num,orderNum);
+                    }
+
+
+                }
+
 
 
 
                 TextView tv_jilu_money=itemView.findViewById(R.id.tv_jilu_money);
                 if(qianBaoListBean.cwallet_amount>0){
-                   // UiTool.setTextColor(tv_jilu_money,R.color.tv_hongse);
+                    UiTool.setTextColor(tv_jilu_money,R.color.tv_hongse);
                     setTextView(itemView,R.id.tv_jilu_money,"+"+ Common.getPriceYB(qianBaoListBean.cwallet_amount));
                 }else {
-                   // UiTool.setTextColor(tv_jilu_money,R.color.tv_lvse_sheng);
+                    UiTool.setTextColor(tv_jilu_money,R.color.tv_lvse_sheng);
                     setTextView(itemView,R.id.tv_jilu_money, Common.getPriceYB(qianBaoListBean.cwallet_amount));
                 }
 
